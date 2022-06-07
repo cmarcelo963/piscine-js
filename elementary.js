c30185e79d7325c9c627883ae29d566d409cc0ba
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-07 12:59:44 +0100

    Elementary

--- elementary.js
+++ elementary.js
@@ -44,14 +44,36 @@ function divide(a, b) {
 
 function modulo(a, b) {
     let result = 0
-    for (let i = a; i > 0; i -= b) {
-        result = i
-        if (b === result) {
-            result = 0
+    if (b === 0) {
+        return
+    }
+    if (a > 0 && b > 0) {
+        for (let i = a; i > 0; i -= b) {
+            result = i
+            if (b === result) {
+                result = 0
+            }
+        }
+    }
+    if (a > 0 && b < 0) {
+        //          
+        for (let i = a; i > 0; i -= b) {
+            result = i
+            if (b === result) {
+                result = 0
+            }
+        }
+    }
+    if (a < 0 && b < 0) {
+        for (let i = a; i < 0; i -= b) {
+            result = i
+            if (b === result) {
+                result = 0
+            }
         }
     }
     return result
 }
 // console.log(multiply(3, -2))
 // console.log(divide(-32, -8))
-// console.log(modulo(26, 6))
+console.log(modulo(-26, -6))
