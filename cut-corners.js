b7c92695cf97f2b722901eef6ba51a60c32fa86e
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-07 19:13:57 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -10,12 +10,16 @@ function round(value) {
         for (let a = newVal; a < -100; a += 100) {
             counter--
             newVal = a
+    // console.log(1)
+
         }
         newVal += 100
     } else {
         for (let a = newVal; a > 100; a -= 100) {
             counter++
             newVal = a 
+    // console.log(2)
+
         }
         newVal -= 100
     }
@@ -34,7 +38,7 @@ function round(value) {
 
 }
 function ceil(value) {
-    console.log(value)
+    // console.log(value)
     if (value === 0) {
         return value
     }
@@ -47,11 +51,15 @@ function ceil(value) {
     if (value > 0) {
         for (let a = newVal; a > 100; a -= 100) {
             counter++
+    // console.log(3)
+
         }
         return counter + 1
     } else if (value < 0) {
         for (let a = newVal; a < -100; a += 100) {
             counter--
+    // console.log(10)
+
             // console.log(1)
             // console.log("here")
         }
@@ -70,24 +78,40 @@ function floor(value) {
     if (value > 0) {
         for (let a = newVal; a > 100; a -= 100) {
             counter++
+    // console.log(5)
+
         }
         return counter
     } else if (value < 0) {
         for (let a = newVal; a < -100; a += 100) {
             counter--
+    // console.log(6)
+
         }
         // console.log("here")
         return counter -1
     }
 }
 function trunc(value) {
+    
+    let counter = 0
+    let n 
+    if (value > 0xfffffffff) {
+        n = value - 0xfffffffff
+        for (let a = n; a > 100; a -= 100) {
+            counter++
+        }
+        return counter + 0xfffffffff
+    }
+    // trunc(value)
+    // console.log(value)
     if (value === 0) {
         return value
     }
 
     let newVal = value * 100
-    let counter = 0
     if (value > 0) {
+        // console.log(8)
         for (let a = newVal; a > 100; a -= 100) {
             counter++
         }
@@ -95,13 +119,15 @@ function trunc(value) {
     } else if (value < 0) {
         for (let a = newVal; a < -100; a += 100) {
             counter--
+
         }
     }
         // console.log("here")
         return counter
+
 }
-console.log(ceil(3.141592653589793))
-console.log(ceil(-3.141592653589793))
-console.log(ceil(2.718281828459045))
-console.log(ceil(-2.718281828459045))
-console.log(ceil(-0))
+// console.log(ceil(3.141592653589793))
+// console.log(ceil(-3.141592653589793))
+// console.log(ceil(2.718281828459045))
+// console.log(ceil(-2.718281828459045))
+// console.log(trunc(8734567236782365783482365))
