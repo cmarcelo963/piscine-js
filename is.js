36244c3373feb577f78c2a3bf5e8f724ad535e1f
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-07 10:55:59 +0100

    Rpeat

--- is.js
+++ is.js
@@ -1,7 +1,7 @@
 
 is.num = a => typeof a === 'number'
-is.nan = a => Number.isNaN(a)
 is.str = a => typeof a === 'string'
+is.nan = a => Number.isNaN(a)
 is.bool = a => a === true || a === false
 is.undef = a =>  a === undefined
 is.def = a => a !== undefined
--- repeat.js
+++ repeat.js
@@ -1,7 +1,16 @@
 function repeat(a, b) {
+
+    is.num = a => typeof a === 'number'
+    is.str = a => typeof a === 'string'
+    if (!b.is.num) {
+        return
+    }
+    if (!a.is.str) {
+        return
+    }
     let toPrint = ""
     for (i = b; i > 0; i--) {
         toPrint += a
     }
-    console.log(toPrint)
+   console.log(toPrint)
 }
