05f3c72dc76b8cd0777c63a01d49c5f0e7e2e716
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-07 14:10:02 +0100

    Elem

--- elementary.js
+++ elementary.js
@@ -1,42 +1,65 @@
 function multiply(a, b) {
     let result = 0
 
-    if (b === 0) {
+    if (b === 0 || a === 0) {
         return 0
     }
+    if (a < 0) {
+        for (let i = a; i < 0; i++) {
+            result -= b
+        }
+    }
+    if (b < 0) {
+        for (let i = b; i < 0; i++) {
+            result -= a
+        }
+    }
 
-    if (a >= 0 && b >= 0) {
+    if (a < 0 && b < 0) {
+        for (let i = b; i !== 0; i++) {
+            result += a
+        }
+    }
+
+    if (a > 0 && b > 0) {
         for (let i = b; i !== 0; i--) {
             result += a
         }
     }
 
-    if (a < 0 && b < 0 ||
-        a < 0 || b < 0) {
-        for (let i = b; i !== 0; i++) {
-            result -= a
-        }
-    }
 
     return result
 }
 
 function divide(a, b) {
     let result = 0
+    if (b > a) {
+        result = 0
+    }
     if (a > 0 && b > 0) {
-        for (let i = a; i !== 0; i-=b) {
+        for (let i = a; i > b; i-=b) {
             result += 1
+        
         }
     }
     if (a > 0 && b < 0) {
-        for (let i = a; i !== 0; i+=b) {
+        //           123     -22     
+        for (let i = a; i > 0; i += b) {
             result -= 1
         }
+        result += 1
+    }
+    if (a < 0 && b > 0) {
+        for (let i = a; i < 0; i += b) {
+            result -=1
+        }
+        result += 1
     }
     if (a < 0 && b < 0) {
-        for (let i = a; i !== 0; i -= b) {
+        for (let i = a; i < 0; i -= b) {
             result += 1
         }
+        result -= 1
     }
 
     return result
@@ -47,6 +70,9 @@ function modulo(a, b) {
     if (b === 0) {
         return
     }
+    if (b > a) {
+         result = a
+    }
     if (a > 0 && b > 0) {
         for (let i = a; i > 0; i -= b) {
             result = i
@@ -56,12 +82,16 @@ function modulo(a, b) {
         }
     }
     if (a > 0 && b < 0) {
-        //          
-        for (let i = a; i > 0; i -= b) {
+        for (let i = a; i > 0; i += b) {
             result = i
             if (b === result) {
                 result = 0
             }
+        }
+    }
+    if (a < 0 && b > 0) {
+        for (let i = a; i < 0; i += b) {
+            result = i
         }
     }
     if (a < 0 && b < 0) {
@@ -74,6 +104,6 @@ function modulo(a, b) {
     }
     return result
 }
-// console.log(multiply(3, -2))
-// console.log(divide(-32, -8))
-console.log(modulo(-26, -6))
+// console.log(multiply(-123, -22))
+// console.log(divide(-123, -22))
+console.log(modulo(-123, 22))
