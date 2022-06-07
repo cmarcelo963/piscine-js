34c2e22c8d633c2434f0bf4431f9c530ceb05c04
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-07 11:01:32 +0100

    Rpeat

--- repeat.js
+++ repeat.js
@@ -1,16 +1,8 @@
 function repeat(a, b) {
 
-    is.num = a => typeof a === 'number'
-    is.str = a => typeof a === 'string'
-    if (!b.is.num) {
-        return
-    }
-    if (!a.is.str) {
-        return
-    }
     let toPrint = ""
     for (i = b; i > 0; i--) {
         toPrint += a
     }
-   console.log(toPrint)
+   return toPrint
 }
