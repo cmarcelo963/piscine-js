a9a75d9d81cba6478e9e09ac109551e0c28ecce1
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-07 11:42:52 +0100

    Elementary

--- elementary.js
+++ elementary.js
@@ -1,11 +1,21 @@
 function multiply(a, b) {
-    return a * b
+    let result = 0
+    for (let i = b; i > 0; i--) {
+        result += a
+    }
+    return result
 }
 
 function divide(a, b) {
-    return a / b
+    let result
+    for (let i = a; i > b; i - b) {
+        result += 1
+    }
+    return result
 }
 
 function modulo(a, b) {
     return a % b
 }
+
+console.log(divide(21, 7))
