d567b95b4a06b50ead3dc7ef35812aec9c2bc1cc
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-09 14:26:14 +0100

    Unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,38 +1,46 @@
-function split(str, val) {
+// console.log(split('a b c', ' '))
+// console.log(split('ggg - ddd - b', ' - '))
+// console.log(split('rrrr', 'rr'))
+console.log(split('Riad', ''))
+// console.log(split('ee,ff,g,', ','))
+
+function split(str, pos) {
     let arr = []
-    let i = 0
+    let temp = ""
+    let index = 0
+    if (pos.length === 0) {
+        for (let a = 0; a < str.length; a++) {
+            arr.push(str.slice(a, a+1))
+        }
+         return arr
+    }
     for (let a = 0; a < str.length; a++) {
-        if ((val === str[a]) || 
-            ((a+1) === str.length)) {
-            let newStr = ""
-            if (a+1 === str.length) {
-                newStr += str[i]
+        console.log(a)
+        if (str.slice(a, a+pos.length) === pos) {
+            if (str.slice(index, a) === "") {
+                arr.push("")
+            } else {
+                arr.push(str.slice(index, a))
             }
-            console.log(i)
-            for (let b = i; b <= a; b++) {
-                // console.log(str[b])
-                newStr += str[b]
+            index = a+pos.length
+            if (pos.length !== 0) {
+                a = a+pos.length-1
             }
-            i = a + 1
-            arr.push(newStr)
         }
     }
-    return arr
+    if (pos !== str.slice(index, str.length)) {
+        arr.push(str.slice(index, str.length))
+    } else if (pos === str.slice(index, str.length)) {
+        arr.push("")
+    }
+    return arr 
 }
 
-console.log(split("ggg - ddd - b', ' - "))
-
-function reverse(value) {
-    let str = ""
-    let arr = []
-    if (Array.isArray(value)) {
-        for (let a = value.length-1; a >= 0; a--) {
-            arr.push(value[a])
-        }
-        return arr
+function join(arr, val) {
+    let res = ""
+    for (let a = 0; a < arr.length-1; a++) {
+        res += arr[a] + val
     }
-    for (let a = value.length-1; a >= 0; a--) {
-        str += value[a]
-    }
-    return str
+    res += arr[arr.length-1]
+    return res
 }
