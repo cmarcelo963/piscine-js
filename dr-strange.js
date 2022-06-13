61c24ca6c32084f8f8cc0b7c12bb54cd67d24ed5
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-13 15:13:27 +0100

    Dr Strange

--- dr-strange.js
+++ dr-strange.js
@@ -1,10 +1,10 @@
-const addWeek = (current) => {
-    const Week = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+function addWeek(current) {
+    const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     let day = ((current.getTime()-new Date('0001-01-01').getTime())/(1000*3600*24))%14
-    return day <= 6 ? Week[day] : 'second'+Week[day-Week.length]
+    return day <= 6 ? week[day] : 'second'+week[day-week.length]
 }
 
-const timeTravel = ({ date, hour, minute, second }) => {
-    date.setHours(hour,minute,second)
+function timeTravel({date, hour, minute, second}) {
+    date.setHours(hour, minute, second)
     return date
 }
