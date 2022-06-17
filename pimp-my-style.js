62cbe8d49969ccf731bac6aa713eb3ce1ef62c3f
cmarcelo963 <ianmarcelo963@gmail.com>
2022-06-18 00:33:37 +0100

    Pimp my Style

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,30 +1,21 @@
-import {styles} from './data.js'
-let i = 0
-let j = 14
-let k = 0
-export function pimp(){
-    let elem = document.querySelector("button")
-    let mat = Math.floor(i/styles.length)
-    console.log(elem.classList)
-    console.log(j)
-        if (mat%2 == 0) {
-            j = 14
-            elem.classList.add(styles[k])
-            i++
-            k++
-            if(k == 15){
-                elem.classList.add('unpimp')
-            }
+import { styles } from './data.js'
+let click = 0
+const pimp = () => {
+    let b = document.querySelector('button')
+    if (b.classList.contains('unpimp')){  
+        click--
+        if (click == 0) {
+            b.classList.remove(styles[click], 'unpimp')
+        } else {
+            b.classList.remove(styles[click])
         }
-        if (mat%2 != 0){
-            k = 0
-            elem.classList.add('unpimp')
-            elem.classList.remove(styles[j])
-            i++
-            j--
-            if(j == -1){
-                elem.classList.remove('unpimp')
-            }
-            
-     }
+    } else {  
+        if (click == 14) {
+             b.classList.add(styles[click], 'unpimp')
+        } else {
+            b.classList.add(styles[click])
+        }
+        click++
+    }
 }
+export {pimp}
