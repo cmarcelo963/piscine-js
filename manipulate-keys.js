d973187e0576fb1493c72a13b0715dbc46275c79
cmarcelo963 <cmarcelo963@yahoo.com>
2022-06-23 12:12:34 +0100

    Debounce

---
+++ manipulate-keys.js
@@ -0,0 +1,17 @@
+const filterKeys = (obj,func) => {
+    let res = {}
+    for (let [k, v] of Object.entries(obj)) {
+        if (func(k)) {
+            res[k] = v
+        }
+    }
+    return res
+}
+
+const mapKeys = (obj,func) => {
+    let res = {}
+    for (let [k, v] of Object.entries(obj)) {
+        res[func(k)] = v 
+    }
+    return res
+}
